3a855d9835413ca780b79f2c64c8c149816266ca
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2021-09-14 18:22:49 +0200

    Pogramado todo lo necesario para represnetar laberintos

--- visorLaberintos/code/routes/index.js
+++ visorLaberintos/code/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'code/uploads/' });
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {});
 });
+
+/*router.post('/stats', upload.single('uploaded_file'), function (req, res) {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any 
+  console.log(req.file, req.body)
+});*/
+
+
+
 
 module.exports = router;
